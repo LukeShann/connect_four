db456c582ea00964038e9c4881fcf0f999b18c5a
Luke <luke.goddard44@gmail.com>
2022-08-02 14:04:26 +0100

    Add Game init tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,16 @@
 require_relative '../lib/Game'
 
 describe Game do
+  context 'Creating a new game' do
+    subject { Game.new }
+
+    xit 'Creates a new board object' do
+    end
+
+    xit 'Assigns board to an instance variable' do
+    end
+
+    xit 'Sets starting player as yellow' do
+    end
+  end
 end
